b4db973972c39cc21bfeded87abfee34e4478f40
William Luck <luck.william.h@gmail.com>
2022-11-02 15:21:50 -0300

    commit tree af4950d4a69f9897b696bb3f2e356d2c53b5fb78

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,24 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+
+
+    if !!params[:name]
+      student = Student.find_by(first_name: params[:name].capitalize)
+      if !student
+        student = Student.find_by(last_name: params[:name].capitalize)
+      end
+      render json: student
+    else 
+      students = Student.all
+      render json: students
+    end
+
+  end
+
+  def record
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
